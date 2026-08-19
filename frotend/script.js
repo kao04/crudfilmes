bb47088b2458c6799ba35f19d0a94f470b9dbefd
EA - Kalebe Sant'Ana <kalebe_santana@educadventista.org>
2026-08-19 07:47:59 -0300

    arrumando

---
+++ frotend/script.js
@@ -0,0 +1,22 @@
+async function buscarFilmes() {
+
+    const resposta = await fetch("https://crudfilmes-6cbnosdfk-kalebes-projects-1a5fd7c2.vercel.app/")
+    const filmes = await resposta.json()
+
+    const lista = document.getElementById(".filmes")
+    lista.innerHTML = ""
+
+    filmes.forEach((filme) => {
+        lista.innerHTML += `
+		<div>
+        <h2>${filme.name}</h2>
+        <p><strong>Gênero: ${filme.genero}</strong></p>
+        <p><strong>Duração: ${filme.duracao}</strong></p>
+        <p><strong>Clasificação: ${filme.classificacao > "0" ? filme.classificacao + 'anos' : 'livre'}</strong></p>
+        </div>
+                `
+        console.log(filme)
+    })
+}
+
+buscarFilmes()
